06de911f270275d20a5303d2a306c1bdd662c55c
anonymous <127376439+anonymous122912@users.noreply.github.com>
2024-03-22 02:25:39 +0530

    Update SocketProvider.jsx

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -9,7 +9,7 @@ export const useSocket = () => {
 };
 
 export const SocketProvider = (props) => {
-  const socket = useMemo(() => io("localhost:3001"), []);
+  const socket = useMemo(() => io("https://sketch-sync-backend-production.up.railway.app/"), []);
 
   return (
     <SocketContext.Provider value={socket}>
